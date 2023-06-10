c47af873c25fd7799a0b6d6ce00a94c6f5980eea
jayfeather <jayfeather233@gmail.com>
2023-06-10 11:52:05 +0800

    add inodes

---
+++ src/gas.h
@@ -0,0 +1,33 @@
+#ifndef __GAS_H__
+#define __GAS_H__
+
+#include <linux/types.h>
+
+struct gas_super_block {
+    __le32 s_magic;
+    __le32 s_blocksize;
+    __le32 s_bam_blocks;
+    __le32 s_iam_blocks;
+    __le32 s_inode_blocks;
+    __le32 s_nblocks;
+    __le32 s_ninodes;
+};
+
+struct gas_inode {
+    __le16 i_mode;
+    __le16 i_nlink;
+    __le32 i_uid;
+    __le32 i_gid;
+    __le32 i_size;
+    __le32 i_atime;
+    __le32 i_mtime;
+    __le32 i_ctime;
+    __le32 i_blkaddr[9];    //6+1+1+1
+};
+
+struct gas_dir_entry {
+    char de_name[SFS_MAX_NAME_LEN];
+    __le32 de_inode;
+};
+
+#endif
